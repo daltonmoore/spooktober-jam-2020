4e8824394a71236ca378720e4c3cc2ba793c86b3
L33txXxtaco <73680948+L33txXxtaco@users.noreply.github.com>
2020-10-30 16:47:59 -0700

    Add files via upload

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Security.Cryptography;
+using System.Collections.Specialized;
+using System.Threading;
+
+public class AIController : MonoBehaviour
+{
+    public int moveSpeed;
+    public int maxDistance;
+    public Transform target;
+    public bool trigger;
+    private Transform myTransform;
+    Animator anim;
+
+    void Awake()
+    {
+
+        myTransform = transform;
+    
+    }
+
+        void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D  other)
+    {
+        if (other.tag == "Player")
+        {
+            target = other.transform;
+
+            maxDistance = 0;
+            trigger = true;
+        }
+    }
+   
+
+
+    void Update()
+    {
+       
+        if (trigger == true)
+        {
+            Debug.DrawLine(target.position, myTransform.position, Color.yellow);
+
+            if (Vector3.Distance(target.position, myTransform.position) > maxDistance)
+            {
+                myTransform.position = Vector3.MoveTowards(myTransform.position, target.position, moveSpeed * Time.deltaTime);
+                anim.SetTrigger("startWalk");
+            }
+        }
+       
+    }
+}
---
+++ Assets/Scripts/HurtPlayer.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HurtPlayer : MonoBehaviour
+{
+    private HealthManager healthMan;
+    public float waitToHurt = 2f;
+    public bool isTouching;
+    [SerializeField]
+    private int damageToGive = 10;
+
+
+    void Start()
+    {
+        healthMan = FindObjectOfType<HealthManager>();
+    }
+
+    void Update()
+    {
+       // if (reloading)
+        //{
+          //  waitToLoad -= Time.deltaTime;
+            //if (waitToLoad <= 0)
+            //{
+              //  SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            //}
+        //}
+
+        if (isTouching)
+        {
+            waitToHurt -= Time.deltaTime;
+            if (waitToHurt <= 0)
+            {
+                healthMan.HurtPlayer(damageToGive);
+                waitToHurt = 2f;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag == "Player")
+        {
+
+            // other.gameObject.SetActive(false);
+            other.gameObject.GetComponent<HealthManager>().HurtPlayer(damageToGive);
+            // reloading = true;
+            
+        }
+
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.collider.tag == "Player")
+        {
+            isTouching = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.collider.tag == "Player")
+        {
+            isTouching = false;
+            waitToHurt = 2f;
+        }
+    }
+
+}
