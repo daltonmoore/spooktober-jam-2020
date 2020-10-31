5702a7d1715f2f985a41aa6236ca0f681c2305fe
daltonmoore <daltonraymoore@gmail.com>
2020-10-31 02:01:50 -0400

    placed enemies and added town, finished map

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    AudioSource audioSource;
+    [SerializeField]
+    AudioClip islandTheme, townTheme;
+    bool islandThemeIsPlaying = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = islandTheme;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            if (islandThemeIsPlaying)
+            {
+                audioSource.clip = townTheme;
+                islandThemeIsPlaying = false;
+            }
+            else
+            {
+                audioSource.clip = islandTheme;
+                islandThemeIsPlaying = true;
+            }
+
+            audioSource.Play();
+        }
+    }
+}
